c910a1f6fabc2dab75d7db059058b7ef3c9fd2bc
philippe <huhmiel@free.fr>
2018-07-13 16:09:36 +0200

    exemple route

--- route.js
+++ route.js
@@ -1,11 +1,31 @@
-module.exports = function(app){
+module.exports = function (app) {
+    const mysql = require('mysql');
+    var con = mysql.createConnection({
+        host: "huhmiel.heliohost.org",
+        user: "huhmiel",
+        password: "SimplonERN@76",
+        database: "huhmiel_CoopCafe"
+    });
 
-    app.get('/profil', function(req, res){
-        
-        res.render('profil') 
-            
-            //title: 'Express Login'
+    //Genere un log lors des erreurs sql
+    con.on('error', function (err) {
+        //console.log("[mysql error]", err);
+    });
+
+    app.get('/profil', function (req, res) {
+        let sqlavis = `SELECT * FROM User WHERE pseudo = 'phil'`;
+        con.query(sqlavis, function (err, resultavis) {
+            console.log(resultavis)
+            if (err) throw err;
+            console.log('avis recupéré');
+            res.render('profil', {
+                utilisateurs: resultavis[0]
+            })
         });
-    };
+        
 
-    //other routes..
+        //title: 'Express Login'
+    });
+};
+
+//other routes..
