9887b6d26f399dfdcdc170115a25436a82e4cced
Vanoux <vanessa.roux891@orange.fr>
2018-07-13 16:11:52 +0200

    je commit!

--- route.js
+++ route.js
@@ -1,11 +1,17 @@
-module.exports = function(app){
+module.exports = function (app) {
 
-    app.get('/profil', function(req, res){
-        
-        res.render('profil') 
-            
+    app.get('/profil', function (req, res) {
+        console.log(req)
+        let sqlavis = `SELECT * FROM User WHERE pseudo = 'phil'` ;
+        con.query(sqlavis, function (err, resultavis) {
+            if (err) throw err;
+            console.log(resultavis);
+            res.render('profil', {
+                users: resultavis
+            })
+
             //title: 'Express Login'
         });
-    };
+    });
 
-    //other routes..
+}
